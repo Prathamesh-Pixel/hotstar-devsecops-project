6a6d89291274ef25ebbfa308b9d9f8eac9214bce
Pratham <edu.jadhav95@gmail.com>
2026-04-19 23:39:01 +0530

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
-import './Banner.css'
-import  { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import './Banner.css';
 
 
 function Banner() {
